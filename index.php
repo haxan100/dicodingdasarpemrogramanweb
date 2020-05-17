93c24bf5f9c839c92b7b9dc532f28f2db8ecfa21
haxan100 <abdul.hasan388@gmail.com>
2020-05-17 20:49:15 +0700

    Belajar Dasar Pemrograman Web

---
+++ index.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+    <head>
+        <style>
+            .box {
+                height: 300px;
+                width: 300px;
+                background-color: #11C5C6;
+            }
+
+            p {
+                height: 75%;
+                width: 75%;
+                background-color: #FBDD1C;
+            }
+
+            .content {
+                max-width: 800px;
+                height: 400px;
+                margin: 0 auto;
+                background-color: deeppink;
+            }
+        </style>
+
+    </head>
+
+<body>
+    <header>
+        <div class="jumbotron">
+            <h1>Bandung</h1>
+            <p>Kota metropolitan terbesar di Provinsi Jawa Barat, sekaligus menjadi ibu kota provinsi tersebut.</p>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="#sejarah">Sejarah</a></li>
+                <li><a href="#geografis">Geografis</a></li>
+                <li><a href="#wisata">Wisata</a></li>
+            </ul>
+        </nav>
+    </header>
+
+
+    <img src="assets/image/history.jpg" class="featured-image" alt="sejarah">
+
+    ...
+
+    <img src="assets/image/geografis.jpg" class="featured-image" alt="geografis">
+
+    ...
+
+    <img src="assets/image/farm-house.jpg" class="featured-image" alt="farm house">
+
+    ...
+
+    <img src="assets/image/bosscha.jpg" class="featured-image" alt="bosscha">
+</body>
+
+</html>
---
+++ mision/assets/js/js.js
@@ -0,0 +1,80 @@
+function myFunction() {
+    // console.log("text");
+    var x = document.getElementById("mainstwo");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function personal() {
+    // console.log("text");
+    var x = document.getElementById("exp");
+    var y = document.getElementById("tambahan_edu");
+    // var x = document.getElementById("personal");
+    var z = document.getElementById("me");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        y.style.display = "block";
+        z.style.display = "block";
+    } else {
+        x.style.display = "none";
+        y.style.display = "none";
+        z.style.display = "none";
+    }
+}
+
+function All() {
+    // console.log("text");
+    var x = document.getElementById("exp");
+    var y = document.getElementById("tambahan_edu");
+    var o = document.getElementById("personal");
+    var z = document.getElementById("me");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        y.style.display = "block";
+        z.style.display = "block";
+        o.style.display = "block";
+    } else {
+        x.style.display = "none";
+        y.style.display = "none";
+        z.style.display = "none";
+        o.style.display = "none";
+    }
+}
+function me() {
+    // console.log("text");
+    var x = document.getElementById("exp");
+    var y = document.getElementById("tambahan_edu");
+    var o = document.getElementById("personal");
+    // var z = document.getElementById("me");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        y.style.display = "block";
+        // z.style.display = "block";
+        o.style.display = "block";
+    } else {
+        x.style.display = "none";
+        y.style.display = "none";
+        // z.style.display = "none";
+        o.style.display = "none";
+    }
+}
+function Experience() {
+    // console.log("text");
+    // var x = document.getElementById("exp");
+    var y = document.getElementById("tambahan_edu");
+    var o = document.getElementById("personal");
+    var z = document.getElementById("me");
+    if (y.style.display === "none") {
+        // x.style.display = "block";
+        y.style.display = "block";
+        z.style.display = "block";
+        o.style.display = "block";
+    } else {
+        // x.style.display = "none";
+        y.style.display = "none";
+        z.style.display = "none";
+        o.style.display = "none";
+    }
+}
